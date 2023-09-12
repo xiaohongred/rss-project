5dfe7777a56aa28bfc654a292845a8cb8e94fb9c
xiaohong <15200689849@163.com>
2023-09-12 23:00:57 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/xiaohongred/rss-project
+
+go 1.20
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness)
+	router := chi.NewRouter()
+
+	router.Mount("/v1", v1Router)
+	router.Get("/", handlerReadiness)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
